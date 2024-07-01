936a4d1a729c968d47635b56ac4c7045df3e32a2
sakshibendre25 <bendresakshi0@gmail.com>
2024-07-01 13:07:49 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,18 @@
+//coverting string into lowercase
+
+
+public class String2 {
+    
+   
+    public static void main(String[] args) {
+        
+        String str = "WELCOME TO CODING!";
+
+        // Convert the  string to  lowercase.
+        String lowerStr = str.toLowerCase();
+
+      
+        System.out.println("Original String: " + str);
+        System.out.println("String in lowercase: " + lowerStr);
+    }
+}
