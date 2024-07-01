aed0bfe13852aba0ab1bf4cfb50b13714d5207ab
sakshibendre25 <bendresakshi0@gmail.com>
2024-07-01 13:07:29 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,19 @@
+
+// converting string into uppercase
+
+
+public class String1 {
+ 
+    
+   public static void main(String[] args) {
+        
+        String str = "welcome to coding!";
+
+        // Convert the  string to uppercase.
+        String upper_str = str.toUpperCase();
+
+       
+        System.out.println("Original String: " + str);
+        System.out.println("String in uppercase: " + upper_str);
+    }
+}
