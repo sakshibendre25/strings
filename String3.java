f63f66f30e2cab04bb2e1150c6abd4c5186789e8
sakshibendre25 <bendresakshi0@gmail.com>
2024-07-01 13:08:10 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,16 @@
+
+public class String3 {
+
+    
+    public static void main(String[] args) {
+        
+        String str = " Hello Java";
+
+        // Trim the whitespace from  front and back of the String.
+        String new_str = str.trim();
+
+        
+        System.out.println("Original String: " + str);
+        System.out.println("New String: " + new_str);
+    }
+}
