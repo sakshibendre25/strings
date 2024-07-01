9cd0146ca0c3ec9ee9419e21f9c9876110348b0b
sakshibendre25 <bendresakshi0@gmail.com>
2024-07-01 13:10:09 +0530

    Add files via upload

---
+++ String7.java
@@ -0,0 +1,13 @@
+//get the length of the string
+
+
+public class String7 {
+   
+    public static void main(String[] args) {
+        String str = "My name is Sakshi";
+       
+        int len = str.length();
+      
+        System.out.println("The string length of '" + str + "' is: " + len);
+    }
+}
